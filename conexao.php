8f6faccfe3a24bbd64d3614a77153a17cb29a740
TalitaLira <lira.talita@hotmail.com>
2016-03-07 00:29:09 -0300

    conexao com o banco(exige senha)

--- conexao.php
+++ conexao.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
 
 	// Conectando com o banco
-	$conexao = mysqli_connect("localhost", "root", "", "loja");
-=======
 	$conexao = mysqli_connect("localhost", "root", "", "loja")  or die(mysqli_connect_error());
->>>>>>> dc054fd1a38c1fc122ecfbb8b3d062221c0e0ce2
 
 ?>
